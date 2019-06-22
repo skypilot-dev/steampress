e4342a57a3144aef35ff7fccb0cb36d74d2b325e
William Thorsen <william@thorsen.io>
2019-06-22 12:06:36 -0600

    cq: Add convenience functions for writing output to files

---
+++ src/filesystem/writeToFile.ts
@@ -0,0 +1,37 @@
+/* TODO: Support output formats other than JSON */
+
+
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
+
+
+export interface WriteToFileOptions {
+  baseName: string;
+  format?: 'json';
+  outDir: string;
+}
+
+/* -- Helper functions */
+function createDirIfNonexistent(dir: string) {
+  if (!existsSync(dir)){
+    mkdirSync(dir);
+  }
+}
+/* -- End of helper functions */
+
+export function writeToFile(obj, {
+  baseName,
+  // format = 'json',
+  outDir = '.',
+}: WriteToFileOptions) {
+
+  if (!baseName.includes('.')) {
+    baseName += '.json';
+  }
+
+  createDirIfNonexistent(outDir);
+
+  writeFileSync(
+    `${outDir}/${baseName}`,
+    JSON.stringify(obj, undefined, 2),
+  );
+}
