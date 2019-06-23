0cb1856ff6051109ad3e9a75b5d30e04f41ba5e5
William Thorsen <william@thorsen.io>
2019-06-23 12:12:49 -0600

    ft: Can use library of common transformations
    Common transformations are now re-exported from the Sugarbowl library

--- src/transformers/index.ts
+++ src/transformers/index.ts
@@ -1,1 +1,17 @@
-export { removeExtraWhitespace, removeWhitespace } from '@skypilot/sugarbowl';
+/* Make available common transformations from the Sugarbowl library */
+
+export {
+  capitalizeAllWords,
+  capitalizeFirstWord,
+  digitsOnly,
+  nondigitsOnly,
+  removeExtraWhitespace,
+  removeWhitespace,
+  toLowerCase,
+  toUpperCase,
+  trim,
+  trimLeft,
+  trimRight,
+} from '@skypilot/sugarbowl';
+
+export { duplicateProperty } from './duplicateProperty';
